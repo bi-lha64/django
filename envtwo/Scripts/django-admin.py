276b275421094ef2ccf67ce14f5d06d285de694e
wanjiku <wanjikubilha64@gmail.com>
2019-05-21 17:28:25 +0300

    add project

---
+++ envtwo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student.aknewhp007\desktop\pythonclass\djangowork\projecttwo\envtwo\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
